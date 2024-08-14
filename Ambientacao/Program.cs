00a2f0fc7bac12cb45faa6acddb249430b729a61
Tingoio <tingoiogamer@gmail.com>
2024-08-14 11:29:25 -0300

    Ambientacao

---
+++ Ambientacao/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite sua idade: ");
+int idade = Console.ReadLine();
---
+++ ListaExerc/Ex06a/Program.cs
@@ -0,0 +1,25 @@
+﻿// 1. Criar um vetor de valores inteiros
+int tamanho = 15;
+int[] vet = new int[tamanho];
+
+Random rand = new Random();
+// 2. Percorrer o vetor com um laço 
+for(int i = 0; i < vet.Length; i++)
+{
+// 3. Descobrir como gerar um valor aleatório
+    vet[i] = rand.Next(100);
+    Console.Write(vet[i] + "|");
+}
+
+Console.WriteLine("\n");
+// 4. Ordenar o vetor usando o Bubble Sort
+for(int i = 0; i < vet.Length - 1; i++)
+{
+    if(i>i+1)
+    {
+        int aux = vet[i];
+        vet[i] = vet[i+1];
+        vet[i+1] = aux;
+    }
+    Console.Write(vet[i] + "|");
+}
