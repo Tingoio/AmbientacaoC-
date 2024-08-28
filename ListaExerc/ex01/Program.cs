82caa92fe5db21256b3378815699eccb23c542c1
Tingoio <tingoiogamer@gmail.com>
2024-08-28 08:52:41 -0300

    Adição dos exercicios de amb

---
+++ ListaExerc/ex01/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Digite a altura: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite a largura: ");
+double largura = Convert.ToDouble(Console.ReadLine());
+
+double area = altura * largura;
+
+Console.WriteLine("Área do retângulo: " + area);
---
+++ ListaExerc/ex02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite o valor em R$: ");
+
+double valor = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Dolar: " + valor * 5.17);
+Console.WriteLine("Euro: " + valor * 6.14);
+Console.WriteLine("Peso Argentino: " + valor * 0.05);
+
+// 1. Dólar (1 dólar = 5,17 reais)
+// 2. Euro (1 euro = 6,14 reais)
+// 3. Peso argentino (1 peso argentino = 0,05 reais)
