0e1a570bc0a26ed56e97893a7da38847ff4d89cf
Tingoio <tingoiogamer@gmail.com>
2024-08-28 08:26:21 -0300

    Remoção de atalho

--- ListaExerc/Ex06a/obj/Debug/net8.0/Ex06a.AssemblyInfo.cs
+++ ListaExerc/Ex06a/obj/Debug/net8.0/Ex06a.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex06a")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00a2f0fc7bac12cb45faa6acddb249430b729a61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+666d294989cd925a31500e4baa45e3eb365a57f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex06a")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex06a")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
