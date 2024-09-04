59473e74955cb7e972c773d135e419c88b631cdb
Tingoio <tingoiogamer@gmail.com>
2024-09-04 11:31:31 -0300

    Criação e envio de objetos e método POST

---
+++ API/API/Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿namespace API.Models;
+
+public class Produto
+{
+    // C# - Construtor da classe
+    public Produto()
+    {
+        Id = Guid.NewGuid().ToString();
+        CriadoEm = DateTime.Now;
+    }
+    // C# - Criação de atributo, get e set
+    public string? Id { get; set;}
+    public string? Nome { get; set;}
+    public double Preco { get; set;}
+    public int Quantidade { get; set;}
+    public DateTime CriadoEm { get; set; }
+
+    // JAVA - Criação de atributo, get e set
+    // private double preco;
+
+    // public double getPreco()
+    // {
+    //     return preco;
+    // }
+    // public void setPreco(double preco)
+    // {
+    //     this.preco = preco * 3;
+    // }
+}
--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,26 +1,52 @@
+using API.Models;
 //Testar as APIs 
 // - Rest Client - Extensão do VsCode
 // - Postman
 // - Insomnia 
 //MINIMAL APIs
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+List<Produto> produtos = new List<Produto>();
+produtos.Add(new Produto(){Nome = "Notebook", Preco = 5000, Quantidade = 54});
+produtos.Add(new Produto(){Nome = "PC", Preco = 10000, Quantidade = 30});
+produtos.Add(new Produto(){Nome = "Mouse", Preco = 100, Quantidade = 109});
+produtos.Add(new Produto(){Nome = "Headset", Preco = 200, Quantidade = 12});
+
 //EndPoints - Funcionalidades
-//Configurar a URL e o método/verbo
-app.MapGet("/", () => "Funcionalidade numero 1");
+//Request - Configurar a URL e o método/verbo HTTP
+//Response - Retornar os dados (json/xml) e
+app.MapGet("/", () => "API de Produtos");
 
-app.MapGet("/segundafuncionalidade", () => "Funcionalidade numero 2");
 
-app.MapGet("/retornarendereco", () =>
-{
-    dynamic endereco = new {
-        rua = "Parigot de Souza",
-        numero = 5300,
-        cep = "81290-350"
-    };
-    return endereco;
+//GET: /produto/listar
+// app.MapGet("/produto/listar", () => 
+// {
+//     return Results.Ok(produtos);
+// });
+
+//POST: /produto/cadastrar
+app.MapPost("/produto/cadastrar/{nome}", (string nome) => 
+{   
+    //Criar e preencher objeto
+    Produto produto = new Produto();
+    produto.Nome = nome;
+    //Adicionando na lista
+    produtos.Add(produto);
+    return Results.Ok(produtos);
 });
+
+// Impressão de objeto
+// app.MapGet("/retornarendereco", () =>
+// {
+//     dynamic endereco = new {
+//         rua = "Parigot de Souza",
+//         numero = 5300,
+//         cep = "81290-350"
+//     };
+//     return endereco;
+// });
 
 //Criar uma funcionalidade/Endpoint para receber informações
 // - Receber informações pela URL da requisição
@@ -28,3 +54,13 @@ app.MapGet("/retornarendereco", () =>
 // - Guardar as informações em uma lista
 
 app.Run();
+
+// C# - Utilização de get e set
+// Produto produto = new Produto();
+
+// Console.WriteLine("Preço: " + produto.Preco);
+
+// JAVA - Utilização de get e set
+// Produto produto = new Produto();
+// produto.setPreco(5);
+// Console.WriteLine("Preço: " + produto.getPreco());
